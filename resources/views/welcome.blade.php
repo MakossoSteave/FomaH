9407d8c39bd2ceb7c9a6b76569231960f95d142f
steave <makossosteave27@gmail.com>
2021-01-27 14:59:52 +0100

    k

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -448,7 +448,7 @@
 
                             @else
                             <div class="rounded-md shadow">
-                                <a href="/login"
+                                <a href="{{ url('/login') }}"
                                     class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">Connection</a>
                             </div>
                             @if (Route::has('register'))
