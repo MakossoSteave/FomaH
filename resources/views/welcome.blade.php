f56faa6872e7fce67492016e8b6f8edaf7c19fa5
steave <makossosteave27@gmail.com>
2021-01-27 17:35:27 +0100

    Add procfils

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -417,7 +417,8 @@
                             <a href="{{ url('/stagiaireEx') }}"
                                 class="font-medium text-gray-500 hover:text-gray-900">Stagiaire</a>
 
-                            <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Centre de Formation</a>
+                            <a href="{{ url('/centreEx') }}"
+                                class="font-medium text-gray-500 hover:text-gray-900">Centre de Formation</a>
 
                             <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Organisme</a>
                         </div>
