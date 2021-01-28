458e50aea7960d8ab116a5600d2ff0dd782be6d3
steave <makossosteave27@gmail.com>
2021-01-28 12:12:01 +0100

    all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,24 +6,25 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-                <li><a href="{{ route('logout')}}" class="px-4 py-2 block text-gray-900 hover:bg-indigo-400 hover:text-white no-underline hover:no-underline"
-                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();"
-                                                     >Deconnection</a></li>
-                            
+                <li><a href="{{ route('logout')}}"
+                        class="px-4 py-2 block text-gray-900 hover:bg-indigo-400 hover:text-white no-underline hover:no-underline"
+                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">Me Connecter</a>
+                </li>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                        </ul>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+                </ul>
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    {{ __('You are logged in! hehehe') }}
+                    {{ __('Felictiation vous êtes  inscrit !') }}
                 </div>
             </div>
         </div>
