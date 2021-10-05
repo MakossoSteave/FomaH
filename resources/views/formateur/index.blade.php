b744146f2bb6b2e4fa353d31624df286c2f16ed3
EvaBarbaro <barbaro.eva@outlook.fr>
2021-10-05 10:04:53 +0200

    add intranet button to formateur page + layouts changed

--- resources/views/formateur/index.blade.php
+++ resources/views/formateur/index.blade.php
@@ -1,72 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.appFormateur')
 @section('content')
-@if(Auth::user())
+
 
 <div class="relative bg-white">
-
-    <div class="max-w-7xl mx-auto px-4 sm:px-6">
-        <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-
-            <div class="flex justify-start lg:w-0 lg:flex-1">
-                <ul>
-                    <li><i class="fas fa-user"> </i>
-                        <a href="#">
-                            <span
-                                class="group bg-white rounded-md text-blue-500 inline-flex items-center text-base font-medium hover:text-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                {{Auth::user()->name}} </span>
-                        </a>
-                        <div class="absolute top-5 right-0 h-8 w-18 p-4">
-                            <button class="js-change-theme focus:outline-none">🌙</button>
-                        </div>
-                    </li>
-                    <li> <i class="fas fa-envelope text-blue-900 font-medium"></i>
-                        <a href="{{route('message',[Auth::user()->id]) }}">
-                            <span
-                                class="group bg-white rounded-md text-base font-medium hover:text-blue-900 text-blue-500">Messages
-                            </span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <nav class="hidden md:flex space-x-10">
-                <div class="relative">
-                    <i class="fas fa-sliders-h "></i>
-                    <button type="button"
-                        class="group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <a href="{{ route('parametre', [Auth::user()->id]) }}">
-                            <span>Parametre</span>
-                        </a>
-
-                    </button>
-                </div>
-                <div class="relative">
-                    <button type="button"
-                        class="group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <svg class="flex-shrink-0 h-6 w-6 text-indigo-600 " xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                        </svg>
-                        <span>Notification</span>
-                    </button>
-                </div>
-            </nav>
-            <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-
-                <a href="{{ route('logout')}}"
-                    onclick="event.preventDefault();document.getElementById('logout-form').submit();" class=" ml-8
-        whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border
-        border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600
-        hover:bg-indigo-700">
-                    Deconnection
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </div>
-
-    </div>
     <div class="max-w-5xl flex items-center h-auto lg:h-1.5 flex-wrap mx-auto my-32 lg:my-0 p-4">
         <div style="border-left:1px solid #000;height:370px"></div>
 
@@ -95,12 +31,12 @@
                             d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" />
                     </svg>France
                 </p>
-                <p class="pt-8 text-sm">Je suis formateur blablabla
+                <p class="pt-8 text-sm">Je suis formateur
                 </p>
 
                 <div class="pt-12 pb-8">
                     <button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
-                        Disponnible
+                        Disponible
                     </button>
                 </div>
 
@@ -651,17 +587,5 @@
             </div>
         </div>
     </footer>
-
-    @else
-<div class="notification is-danger has-text-centered my-4">
-Votre session a expiré !
-</div>
-<button type="button" class="group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <a href="/">
-                        <i class="fas fa-home"></i>
-                            <span>Acceuil</span>
-                        </a>
-
-</button>
-@endif
+ -->
 @endsection
